f6a7339655307678ca8d69e1bb4dd088cd3ef22e
PCISGroup <info@pcis.group>
2026-03-03 10:41:59 +0200

    Updated Server/Server.js

--- server/server.js
+++ server/server.js
@@ -31,7 +31,8 @@ const PORT = process.env.PORT || 5001;
 
 app.use(cors({
   origin: [
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'https://pcisgroup.com',
   ],
   credentials: true ,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
